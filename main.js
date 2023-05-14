a7336270be6b58eeba0393174dd2090673bb225f
Dilsoor <abdudilsora@gmail.com>
2023-05-14 21:24:31 +0500

    header-fixed

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-
 let myElements = document.querySelectorAll(".rests");
 let myButton = document.getElementById("Adv-show");
 
-myButton.addEventListener("click", function() {
-    myElements.forEach(function(element) {
-      element.classList.toggle("hidden");
-    });
-    myButton.innerHTML = "Qisqartirish"
+myButton.addEventListener("click", function () {
+  myElements.forEach(function (element) {
+    element.classList.toggle("hidden");
+  });
+  myButton.innerHTML = "Qisqartirish";
 });
+
+function myFunction(){ 
+  var playVideo = document.getElementById('myVideo');
+  var button = document.getElementById('myBtn');
+
+  if (playVideo.paused) {
+     playVideo.play();
+      button.innerHTML= "pause";
+  } else {
+     button.innerHTML= "play";
+     playVideo.pause();
+  }
+  };
 
