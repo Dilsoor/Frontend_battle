bbd1b1787f765fffe586dcfada02e2fc270cb231
Dilsoor <abdudilsora@gmail.com>
2023-05-14 22:47:09 +0500

    sdafds

--- main.js
+++ main.js
@@ -14,12 +14,5 @@ console.log(modeBtn);
 modeBtn[0].addEventListener('click',()=>{
   document.querySelector('body').classList.toggle('night');
 })
-// modeBtn[1].addEventListener('click',()=>{
-//   modeBtn[0].classList.remove('active');
-//   modeBtn[1].classList.add('active');
-//   document.querySelector('body').classList.remove('night');
-// })
-// window.addEventListener("scroll", function () {
-//   toggleBacktop();
-// });
+
 
